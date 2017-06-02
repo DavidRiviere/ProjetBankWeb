99808f2fd77cf0554595dd5e3b185a51d0c4bb1e
DavidRiviere <david_riviere@hotmail.fr>
2017-06-02 17:21:26 +0200

    j'ai fait du ménage

--- AngularJSONTest/test-app/src/app/app.module.ts
+++ AngularJSONTest/test-app/src/app/app.module.ts
@@ -7,11 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroService }          from './hero.service';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
 
 import { CpvilleFormComponent }  from './cpville-forms/cpville-forms.component';
 import { CreateaccountComponent }  from './createaccount/createaccount.component';
@@ -25,14 +20,9 @@ import { CreateaccountComponent }  from './createaccount/createaccount.component
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    HeroSearchComponent,
     CpvilleFormComponent,
     CreateaccountComponent
   ],
-  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
