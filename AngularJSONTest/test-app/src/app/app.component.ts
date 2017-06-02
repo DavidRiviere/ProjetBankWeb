6841f9249952a7f19f8897b7886d0919c8da27c7
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-06-02 11:07:12 +0200

    test angular serve OK

--- AngularJSONTest/test-app/src/app/app.component.ts
+++ AngularJSONTest/test-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component }          from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Money bank</h1>
+    <h1>Money bank manager</h1>
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
