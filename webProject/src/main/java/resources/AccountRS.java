a4f05ebd9434ebccbc15d6933f3858096e3005b2
MathieuTraparic <mathieu.traparic@gmail.com>
2017-06-07 10:32:29 +0200

    added inheritance on AccountRS and POST JSON managing

--- webProject/src/main/java/resources/AccountRS.java
+++ webProject/src/main/java/resources/AccountRS.java
@@ -4,10 +4,7 @@ import java.net.URI;
 import java.util.Date;
 import java.util.List;
 
-import javax.ejb.EJB;
-import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -21,35 +18,13 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import biz.PersistManager;
 import builders.AccountBuilder;
 import model.Account;
 import model.Transaction;
 import mvc.model.AccountDoesNotExistException;
 
-//@Path("/{a:rs/account|index.html}")
-public class AccountRS {
-
-	@PersistenceContext(unitName = "bankProjectWeb")
-	private EntityManager entityManager;
-
-	@EJB
-	private PersistManager persistManager;
-
-	// @Resource
-	// private RS rs;
-
-	@GET
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Account get(@PathParam("id") long id) throws AccountDoesNotExistException {
-		try {
-			return entityManager.createQuery("SELECT a FROM Account a WHERE a.id = :id", Account.class)
-					.setParameter("id", id).getSingleResult();
-		} catch (NoResultException e) {
-			throw new AccountDoesNotExistException();
-		}
-	}
+@Path("/rs/{class:account}")
+public class AccountRS extends RS{
 
 	public boolean accountAlreadyExist(String accountNumber) {
 		return (entityManager.createQuery("SELECT Count(a) FROM Account a WHERE a.number = :accountNumber", Long.class)
@@ -58,36 +33,21 @@ public class AccountRS {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response post(Account account, @Context UriInfo uriInfo) {
+	public Response postJson(Account account, @Context UriInfo uriInfo) {
 
 		if (accountAlreadyExist(account.getNumber())) {
 			return Response.status(404).build();
 		}
 
-		persistManager.persist(account);
+		this.persistManager.persist(account);
 
-		URI location = uriInfo.getRequestUriBuilder().path(String.valueOf(account.getId())).build();
-		return Response.seeOther(location).build();
+		return postLocation(account.getId(), uriInfo);
 
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public Response post(MultivaluedMap<String, String> formParams, @Context UriInfo uriInfo) {
-
-		/*
-		 * Account account = new Account(number, description, initialBalance,
-		 * 0.0, 0, 0, (CountryCode) rs.getById(1L, CountryCode.class), new
-		 * Date(), (Agency) rs.getById(1, Agency.class), (AccountType)
-		 * rs.getById(1, AccountType.class));
-		 */
-		/*
-		Account account = new Account();
-		account.setNumber(formParams.getFirst("accountNumber"));
-		account.setDescription(formParams.getFirst("accountDescription"));
-		account.setInitialBalance(Double.valueOf(formParams.getFirst("accountInitialBalance")));
-		account.setCreationDate(new Date());
-		*/
 		
 		Account account = new AccountBuilder().setDescription(formParams.getFirst("accountDescription"))
 				.setCreationDate(new Date())
@@ -101,22 +61,14 @@ public class AccountRS {
 
 		persistManager.persist(account);
 
-		URI location = uriInfo.getRequestUriBuilder().path(String.valueOf(account.getId())).build();
-		return Response.seeOther(location).build();
+		return postLocation(account.getId(), uriInfo);
 
 	}
 
 	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response deleteAccount(Account account) {
 		persistManager.remove(account);
-		return Response.ok().build();
-
-	}
-
-	@DELETE
-	public Response deleteAccountFromID(Long id) throws AccountDoesNotExistException {
-
-		persistManager.remove(get(id));
 		return Response.ok().build();
 
 	}
