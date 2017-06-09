f62cd97ee6b9792d823520159104fd838d63809b
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-06-09 12:05:49 +0200

    removing still some legacy

--- AngularJSONTest/test-app/src/app/app.component.ts
+++ AngularJSONTest/test-app/src/app/app.component.ts
@@ -6,10 +6,8 @@ import { Component }          from '@angular/core';
     <h1>Money bank manager</h1>
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
       <a routerLink="/cpvilleform" routerLinkActive="active">Cpvilleform</a>
       <a routerLink="/createaccountform" routerLinkActive="active">Create Account Form</a>
-      <a routerLink="/account/1/transactions" routerLinkActive="active">List of your transactions</a>
     </nav>
     <router-outlet></router-outlet>
   `,
