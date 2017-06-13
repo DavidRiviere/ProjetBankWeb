380f3874d7a62bf40c9dd63ab4611cb55de6e7e4
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-06-13 17:27:21 +0200

    still working on auth sys angular side

--- AngularJSONTest/test-app/src/app/services/authentication.service.ts
+++ AngularJSONTest/test-app/src/app/services/authentication.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+
+import { User } from  '../model/user';
+
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AuthenticationService {
+
+    user: User = new User();
+
+    private headers = new Headers({'Content-Type': 'application/json'});
     constructor(private http: Http) { }
 
     login(username: string, password: string) {
-        return this.http.post('/api/authenticate', JSON.stringify({ login: username, pswd: password }))
+        this.user.login = username;
+        this.user.pswd = password;
+        return this.http.post('https://localhost:8443/bankProjectWeb/login/', JSON.stringify(this.user),  {headers:this.headers})
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let owner = response.json();
